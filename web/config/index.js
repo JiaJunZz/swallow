fd2ba9bef089d719daf676b96ca9e3b07321bba0
小恐龙 <597105373@qq.com>
2019-04-20 17:01:50 +0800

    Update index.js

--- web/config/index.js
+++ web/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '127.0.0.1', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 9528, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
